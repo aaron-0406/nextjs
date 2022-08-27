cb305e156b580da18395dbbe2962560c04362c94
Aaron Paredes Cabrera <iron0406@outlook.com>
2022-08-27 17:26:56 -0500

    chore: consuming api

--- PROJECTS/next-aguacate/pages/index.tsx
+++ PROJECTS/next-aguacate/pages/index.tsx
@@ -1,11 +1,29 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Navbar from '../components/Navbar/Navbar'
+import Link from 'next/link'
 
 const Home = () => {
+  const [productList, setProductList] = useState<TProduct[]>([])
+
+  useEffect(() => {
+    window
+      .fetch('/api/avo')
+      .then((response) => response.json())
+      .then(({ data, length }) => {
+        setProductList(data)
+      })
+  }, [])
+
   return (
     <div>
       <Navbar />
       <h1>Hello World</h1>
+
+      {productList.map((product) => (
+        <div key={product.id}>
+          <Link href={`/product/${product.id}`}>{product.name}</Link>
+        </div>
+      ))}
     </div>
   )
 }
--- PROJECTS/next-aguacate/pages/product/[id].tsx
+++ PROJECTS/next-aguacate/pages/product/[id].tsx
@@ -1,12 +1,29 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { useRouter } from 'next/router'
 
 const ProductPage = () => {
-  const { query } = useRouter()
+  const [product, setProduct] = useState<TProduct>()
+
+  const {
+    query: { id },
+  } = useRouter()
+
+  useEffect(() => {
+    window
+      .fetch(`/api/avo/${id}`)
+      .then((response) => response.json())
+      .then((data) => {
+        setProduct(data)
+      })
+  }, [id])
 
   return (
     <section>
-      <h1>Página producto: {query.id}</h1>
+      <h1>Página producto:</h1>
+      <p>{product?.id}</p>
+      <p>{product?.name}</p>
+      <p>{product?.price}</p>
+      <img src={product?.image} />
     </section>
   )
 }
