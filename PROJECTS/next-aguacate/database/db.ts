5081b46eb213122f18c80856e6273d023842e1e5
Aaron Paredes Cabrera <iron0406@outlook.com>
2022-08-27 16:25:05 -0500

    chore: add method getAllAvos on API

---
+++ PROJECTS/next-aguacate/database/db.ts
@@ -0,0 +1,32 @@
+import allData from './data'
+
+class Database {
+  constructor() {}
+
+  async getAll(): Promise<TProduct[]> {
+    const asArray = Object.values(allData)
+    await randomDelay()
+    return asArray
+  }
+
+  async getById(id: string): Promise<TProduct | null> {
+    if (!Object.prototype.hasOwnProperty.call(allData, id)) {
+      return null
+    }
+
+    const entry = allData[id]
+    await randomDelay()
+    return entry
+  }
+}
+
+const randomDelay = () =>
+  new Promise((resolve) => {
+    const max = 350
+    const min = 100
+    const delay = Math.floor(Math.random() * (max - min + 1)) + min
+
+    setTimeout(resolve, delay)
+  })
+
+export default Database
